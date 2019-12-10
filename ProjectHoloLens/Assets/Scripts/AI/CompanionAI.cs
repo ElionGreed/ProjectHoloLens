5773fd129a14be2a88f2c4867823000a70723540
MadPengu <36513397+MadPengu@users.noreply.github.com>
2019-12-10 02:25:16 +0000

    Utility AI - first working prototype

--- ProjectHoloLens/Assets/Scripts/AI/CompanionAI.cs
+++ ProjectHoloLens/Assets/Scripts/AI/CompanionAI.cs
@@ -7,32 +7,62 @@ using UnityEngine.AI;
 
 public class CompanionAI : MonoBehaviour
 {
+    [SerializeField]
     float[] utilityValues = new float[5];
     [SerializeField]
-    int companionHealth = 100;
+    float companionHealth = 100;
+    float damage = 10;
+    float distance;
     NavMeshAgent agent;
     GameObject[] enemy;
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject runAwaySpot;
     float[] enemyHealth;
+    int biggestAction;
     OrcScript orcScipt;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        distance = Vector3.Distance(transform.position, player.transform.position);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
         enemy = GameObject.FindGameObjectsWithTag("Enemy");
         CalculateUtility();
     }
     private void CalculateUtility()
     {
-        orcScipt = enemy[0].GetComponent<OrcScript>();
+
+        //orcScipt = enemy[0].GetComponent<OrcScript>();
+
+        utilityValues[0] = 0;  // Death Value
+        if (companionHealth < 1)
+        {
+            utilityValues[0] = 1000;
+        }
+        utilityValues[1] = 100/companionHealth;  // Run Away Value
+        utilityValues[2] = 0;
+        if (enemy.Length != 0)
+        {
+            utilityValues[2] = 100 / (enemy.Length*4); // Attack Value
+        }
+        utilityValues[3] = 0;// Follow Value
+        if (enemy.Length == 0)
+        {
+            utilityValues[0] = 0; // if there are no enemies respawn and follow player
+            utilityValues[3] = 999;
+        }
+        chooseAction();
     }
     private void chooseAction()
     {
-        int biggestAction = 0;
+        biggestAction = 3;
         for (int n = 0; n < 4; n++)
         {
             if (utilityValues[n + 1] > utilityValues[n])
@@ -46,23 +76,22 @@ public class CompanionAI : MonoBehaviour
         {
             case 0:
                 {
-                   // Patrol();
+                    Downed();
                     break;
                 }
             case 1:
                 {
-                    //  anim.SetTrigger("Move");
-                   // MoveToTarget();
+                   RunAway();
                     break;
                 }
             case 2:
                 {
-                    //AttackEnemy();
+                    AttackEnemy();
                     break;
                 }
             case 3:
                 {
-                   // Die();
+                   Follow();
                     break;
                 }
         }
@@ -71,7 +100,26 @@ public class CompanionAI : MonoBehaviour
 
 
     }
+    private void Downed()
+    {
+        agent.isStopped = true;
+    }
+    private void RunAway()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(runAwaySpot.transform.position);
+    }
 
+    private void AttackEnemy()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(enemy[0].transform.position);
+    }
+    private void Follow()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(player.transform.position);
+    }
 }
 
 
