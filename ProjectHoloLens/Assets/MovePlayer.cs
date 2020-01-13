a139c35729499d0790563c787f6f0ddedb3b3eb6
Fadwa <fadwa99@live.com>
2020-01-13 17:13:29 +0000

    grid

---
+++ ProjectHoloLens/Assets/MovePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    Vector3 targetPosition;
+    Vector3 lookAtTarget;
+    Quaternion playerRotation;
+    float rotationSpeed = 5;
+    float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetTargetPosition();
+        }
+        Move();
+    }
+
+    private void SetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            targetPosition = hit.point;
+            lookAtTarget = new Vector3(targetPosition.x - transform.position.x, transform.position.y, targetPosition.z - transform.position.z);
+            playerRotation = Quaternion.LookRotation(lookAtTarget);
+            //this.transform.Translate(targetPosition.x, 0, targetPosition.z);
+        }
+    }
+
+    private void Move()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, playerRotation, rotationSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+}
