f08075d63c649973e0b2dbb37afd4100b4c4fa9a
ElionGreed <56034490+ElionGreed@users.noreply.github.com>
2020-05-07 23:24:48 +0100

    Meh Dungeon gen
    Meh

--- ProjectHoloLens/Assets/Scripts/DungeonGen/Node.cs
+++ ProjectHoloLens/Assets/Scripts/DungeonGen/Node.cs
@@ -1,28 +1,28 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-
 public abstract class Node
 {
+    private List<Node> childrenNodeList;
 
-    private List<Node> childenNodes;
-    public List<Node> ChildenNodes { get => childenNodes; }
+    public List<Node> ChildrenNodeList { get => childrenNodeList;}
 
     public bool Visted { get; set; }
-    public Vector2Int BottomLeftCorner { get; set; }
-    public Vector2Int BottomRightCorner { get; set; }
-    public Vector2Int TopRightCorner { get; set; }
-    public Vector2Int TopLeftCorner { get; set; }
+    public Vector2Int BottomLeftAreaCorner { get; set; }
+    public Vector2Int BottomRightAreaCorner { get; set; }
+    public Vector2Int TopRightAreaCorner { get; set; }
+    public Vector2Int TopLeftAreaCorner { get; set; }
 
-    public int LayerIndex { get; set; }
     public Node Parent { get; set; }
 
-    // Start is called before the first frame update
+
+    public int TreeLayerIndex { get; set; }
+
     public Node(Node parentNode)
     {
-        childenNodes = new List<Node>();
+        childrenNodeList = new List<Node>();
         this.Parent = parentNode;
-        if(parentNode != null)
+        if (parentNode != null)
         {
             parentNode.AddChild(this);
         }
@@ -30,12 +30,12 @@ public abstract class Node
 
     public void AddChild(Node node)
     {
-        childenNodes.Add(node);
+        childrenNodeList.Add(node);
 
     }
 
     public void RemoveChild(Node node)
     {
-        childenNodes.Remove(node);
+        childrenNodeList.Remove(node);
     }
 }
