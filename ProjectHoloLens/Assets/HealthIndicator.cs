46410b57493c4acd821d29703119d5518fbd91b5
Fadwa <fadwa99@live.com>
2020-05-11 07:54:28 +0100

    Added health bar

---
+++ ProjectHoloLens/Assets/HealthIndicator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthIndicator : MonoBehaviour
+{
+    //private Material material;
+
+    //private void Start()
+    //{
+    //    material = GetComponent<Renderer>().material;
+    //    //FindObjectOfType<CommonUnit>().OnHPChanged += HealthIndicator_OnHPChanged;
+
+    //    float pct = 10;
+    //    material.SetFloat("_Cutoff", 1f - pct);
+
+    //}
+
+    //public void HealthIndicator_OnHPChanged(float pct)
+    //{
+    //    print("indicator change dmg");
+
+    //    material.SetFloat("_Cutoff", 1f - pct);
+    //}
+}
--- ProjectHoloLens/Assets/Scripts/AI/CommonUnit.cs
+++ ProjectHoloLens/Assets/Scripts/AI/CommonUnit.cs
@@ -1,39 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CommonUnit : MonoBehaviour
 {
+    [SerializeField]
+    protected int myHealth, mySpeed, myDamage;
+    private int maxHealth = 100;
+    public Image HPBar;
 
-    [SerializeField]
-    protected int myHealth;
-    [SerializeField]
-    protected int mySpeed;
-    [SerializeField]
-    protected int myDamage;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-       
+        myHealth = maxHealth;
+    }
+    private void Start()
+    {
+
     }
 
- 
- 
     public void TakeDamage(int amount)
     {
-        myHealth -= amount;     
-    }
+        myHealth -= amount;
+        HPBar.fillAmount = myHealth / maxHealth;
+        print("taking dmg");
 
-   
+        if (myHealth <= 0)
+        {
+            Die();
+        }
+    }
 
     void Die()
     {
-        if (myHealth <= 0)
-        {
-            UnitManager.unitManager.enemyUnits.Remove(gameObject);
-            UnitManager.unitManager.playerUnits.Remove(gameObject);
-            Destroy(gameObject);
-        }
+        UnitManager.unitManager.enemyUnits.Remove(gameObject);
+        UnitManager.unitManager.playerUnits.Remove(gameObject);
+        Destroy(gameObject);
     }
 }
