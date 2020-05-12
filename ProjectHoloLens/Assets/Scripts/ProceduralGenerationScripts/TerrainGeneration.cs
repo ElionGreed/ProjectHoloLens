2740f8c9d7d33032f19a2e85e31f1a7c247c7875
ElionGreed <56034490+ElionGreed@users.noreply.github.com>
2020-05-12 02:18:20 +0100

    Trees Gen now
    Tree gen

--- ProjectHoloLens/Assets/Scripts/ProceduralGenerationScripts/TerrainGeneration.cs
+++ ProjectHoloLens/Assets/Scripts/ProceduralGenerationScripts/TerrainGeneration.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 public class TerrainGeneration : MonoBehaviour
 {
     public GameObject tree;
-    public GameObject worldItems;
+    public GameObject TreeTwo;
+    public GameObject TreeThree;
 
-    public float minTSize;
-    public float maxTSize;
+    public float minTreeOneSize;
+    public float maxTreeOneSize;
 
-    public float minISize;
-    public float maxISize;
+    public float minTreeTwoSize;
+    public float maxTreeTwoSize;
 
+    public float minTreeThreeSize;
+    public float maxTreeThreeSize;
 
     public Texture2D noiseImage;
 
-    //Enviroment Size
-    public float forestSize;
-    public float worldItemSize;
+    public float TreeSizeOne;
+    public float TreeSizeTwo;
+    public float TreeSizeThree;
 
-    //Density For the Enviroment Items
+
     public float treeDensity;
     private float baseDensity = 5.0f;
     public float itemDensity;
@@ -35,17 +38,17 @@ public class TerrainGeneration : MonoBehaviour
     public void Generate()
     {
 
-        for (int y = 0; y < forestSize; y++)
+        for (int y = 0; y < TreeSizeOne; y++)
         {
 
-            for (int x = 0; x < forestSize; x++)
+            for (int x = 0; x < TreeSizeOne; x++)
             {
 
                 float chance = noiseImage.GetPixel(x, y).r / (baseDensity / treeDensity);
 
                 if (TreePlace(chance))
                 {
-                    float size = Random.Range(minTSize, maxTSize);
+                    float size = Random.Range(minTreeOneSize, maxTreeOneSize);
 
                     GameObject newTree = Instantiate(tree);
                     newTree.transform.localScale = Vector3.one * size;
@@ -56,19 +59,40 @@ public class TerrainGeneration : MonoBehaviour
         }
 
 
-        for (int y = 0; y < worldItemSize; y++)
+        for (int y = 0; y < TreeSizeTwo; y++)
         {
 
-            for (int x = 0; x < worldItemSize; x++)
+            for (int x = 0; x < TreeSizeTwo; x++)
             {
 
                 float chance = noiseImage.GetPixel(x, y).r / (baseDensity / treeDensity);
 
                 if (ItemPlace(chance))
                 {
-                    float size = Random.Range(minISize, maxISize);
+                    float size = Random.Range(minTreeTwoSize, maxTreeTwoSize);
 
-                    GameObject NewItem = Instantiate(worldItems);
+                    GameObject NewItem = Instantiate(TreeTwo);
+                    NewItem.transform.localScale = Vector3.one * size;
+                    NewItem.transform.position = new Vector3(x, 0, y);
+                    NewItem.transform.parent = transform;
+                }
+            }
+        }
+
+
+        for (int y = 0; y < TreeSizeThree; y++)
+        {
+
+            for (int x = 0; x < TreeSizeThree; x++)
+            {
+
+                float chance = noiseImage.GetPixel(x, y).r / (baseDensity / treeDensity);
+
+                if (ItemPlace(chance))
+                {
+                    float size = Random.Range(minTreeThreeSize, maxTreeThreeSize);
+
+                    GameObject NewItem = Instantiate(TreeThree);
                     NewItem.transform.localScale = Vector3.one * size;
                     NewItem.transform.position = new Vector3(x, 0, y);
                     NewItem.transform.parent = transform;
